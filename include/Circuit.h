44168a2c6d542ff294101215b9488e3dcdc3ad3c
Jader <jvjader@gmail.com>
2023-06-02 16:26:05 +0200

    Added Circuit files and completed header

---
+++ include/Circuit.h
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <memory>
+
+#include "Node.h"
+
+typedef std::shared_ptr<Node> NodePtr;
+
+class Circuit
+{
+private:
+    std::map<int, NodePtr> nodes;
+    std::map<std::string, int> nodeNameToIdLookup;
+    std::vector<int> sortedIds;
+    std::vector<int> probes; // not sure if this will work 
+
+public:
+    Circuit();
+    ~Circuit();
+
+    // calculate the output of the circuit
+    // TODO: decide if vector is the best return type
+    int run();
+
+    void addNode(NodePtr node);
+    void sortNodes();
+    int getNodeId(std::string nodeName);
+    void addProbe(std::string nodeName);
+    void printNodes();
+};
