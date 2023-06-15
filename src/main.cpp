c76097397f07d14da08a4253084c1d38f7c7bf98
Jader <jvjader@gmail.com>
2023-06-15 11:39:47 +0200

    cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,10 @@ int main(int argc, char const *argv[])
     std::vector<std::string> nodeNames = {"AND", "OR", "NOT", "NAND", "NOR", "XOR"};
     std::vector<std::shared_ptr<Node>> nodes;
 
-    for (auto str : nodeNames)
-    {
-        std::shared_ptr<Node> node = NodeFactory::create(str, str);
-        nodes.push_back(node);
-    }
 
-    for (auto node : nodes)
-    {
-        if (node != nullptr)
-            std::printf("Node %s\n", node->getName().c_str());
-    }
+    NodePtr node = NodeFactory::create("AND", "test");
+
+    
 
     return 0;
 }
